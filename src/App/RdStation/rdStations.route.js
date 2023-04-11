79f122699a2c55d4ec288deb8d133920ea6b9478
Augusto Silva <gutim160@gmail.com>
2023-04-11 06:30:42 -0300

    refactor: add todas fucionalidades de integração do RD Station

--- src/App/RdStation/rdStations.route.js
+++ src/App/RdStation/rdStations.route.js
@@ -2,11 +2,10 @@ const routesRdStation = require("express").Router();
 const GetAuthUrlController = require("./Controllers/GetAUthUrlController");
 const GetAccessTokenController = require("./Controllers/GetAccessTokenController");
 const CreateContatoController = require("./Controllers/CreateContatoController");
-const AuthMiddleware = require("../Middleware/AuthRdStation");
 
 routesRdStation.get("/getAuthUrl", new GetAuthUrlController().handle);
 routesRdStation.get("/getAccessToken", new GetAccessTokenController().handle);
-routesRdStation.post("/createContato", new AuthMiddleware().handle, new CreateContatoController().handle);
+routesRdStation.post("/createContato", new CreateContatoController().handle);
 
 
 module.exports = routesRdStation;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 
 const rdStationRouter = require('./rdStation.routes')
 const router = require("express").Router();
-
+const routeRDStaion = require('../App/RdStation/rdStations.route')
 
 
 router.use('/', rdStationRouter);
+router.use('/rdStation', routeRDStaion);
 
 module.exports = router
 
